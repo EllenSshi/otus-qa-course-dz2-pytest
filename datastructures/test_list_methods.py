0426a00653161c4f84b419e3df1c94672d36b702
eshenko <alexeyeschenko@gmail.com>
2020-01-18 20:28:24 +0300

    create tests for list methods

--- datastructures/test_list_methods.py
+++ datastructures/test_list_methods.py
@@ -1,18 +1,40 @@
 import pytest
 
 
-# @pytest.mark.parametrize()
-def test_1():
-    pass
+@pytest.mark.lst
+@pytest.mark.parametrize("itm", ["apple", "pineapple", "watermelon"])
+def test_appended_item(itm):
+    testlist = ["item1", "item2", "item3", "item4"]
+    list_len = len(testlist)
+    testlist.append(itm)
+    print(testlist)
+    assert testlist[-1] == itm
 
-def test_2():
-    pass
 
-def test_3():
-    pass
+@pytest.mark.lst
+def test_copied_list():
+    oldlist = ["flowers", "trees", "bushes"]
+    newlist = oldlist.copy()
+    assert oldlist == newlist
 
-def test_4():
-    pass
 
-def test_5():
-    pass
+@pytest.mark.lst
+def test_extended_list():
+    lst1 = ["a1", "a2", "b1", "b2"]
+    lst1_len = len(lst1)
+    lst2 = ["c1", "c2"]
+    lst1.extend(lst2)
+    assert lst1[lst1_len] == lst2[0]
+
+
+@pytest.mark.lst
+def test_cleared_list():
+    testlist = ["one", "two", "three", "four", "five"]
+    testlist.clear()
+    assert len(testlist) == 0
+
+
+@pytest.mark.lst
+def test_item_count():
+    testlist = ["one", "two", "three", "one"]
+    assert testlist.count("one") == 2
--- datastructures/test_set_methods.py
+++ datastructures/test_set_methods.py
@@ -1,18 +1,27 @@
 import pytest
 
 
+@pytest.mark.set
 # @pytest.mark.parametrize()
 def test_1():
     pass
 
+
+@pytest.mark.set
 def test_2():
     pass
 
+
+@pytest.mark.set
 def test_3():
     pass
 
+
+@pytest.mark.set
 def test_4():
     pass
 
+
+@pytest.mark.set
 def test_5():
     pass
