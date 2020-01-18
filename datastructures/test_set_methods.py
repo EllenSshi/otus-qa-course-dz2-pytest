604355a1e21477fc792fbf220fa471fce283ba2b
eshenko <alexeyeschenko@gmail.com>
2020-01-18 20:47:34 +0300

    create tests for set methods

--- datastructures/test_set_methods.py
+++ datastructures/test_set_methods.py
@@ -2,26 +2,39 @@ import pytest
 
 
 @pytest.mark.set
-# @pytest.mark.parametrize()
-def test_1():
-    pass
+@pytest.mark.parametrize("newitem", ["swimming", "knitting", "snowboarding"])
+def test_check_item_in_set(newitem):
+    testset = {"reading", "dancing", "skiing"}
+    testset.add(newitem)
+    assert newitem in testset
 
 
 @pytest.mark.set
-def test_2():
-    pass
+def test_updated_set():
+    testset = {"reading", "dancing", "skiing"}
+    lst = ["swimming", "knitting", "snowboarding"]
+    testset.update(lst)
+    assert lst[1] in testset
 
 
 @pytest.mark.set
-def test_3():
-    pass
+def test_cleared_set():
+    testset = {"reading", "dancing", "skiing"}
+    testset.clear()
+    assert len(testset) == 0
 
 
 @pytest.mark.set
-def test_4():
-    pass
+def test_difference_update_method():
+    testset1 = {"reading", "dancing", "skiing"}
+    testset2 = {"reading", "swimming", "skiing"}
+    testset1.difference_update(testset2)
+    assert "reading" not in testset1
 
 
 @pytest.mark.set
-def test_5():
-    pass
+def test_sets_union():
+    testset1 = {"reading", "dancing", "skiing"}
+    testset2 = {"swimming", "knitting", "snowboarding"}
+    testset3 = testset1.union(testset2)
+    assert testset2.issubset(testset3)
